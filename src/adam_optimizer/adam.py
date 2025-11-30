a952a289e2edc1bf2afd238caf606d7866f03a75
dkoe00 <darius.koenig@outlook.de>
2025-11-30 23:21:18 +0100

    fix bugs

--- src/adam_optimizer/adam.py
+++ src/adam_optimizer/adam.py
@@ -1,8 +1,6 @@
 import math
-import numpy as np
 import torch
 
-from collections import defaultdict
 from typing import Iterable
 
 class Adam(torch.optim.Optimizer):
@@ -20,24 +18,44 @@ class Adam(torch.optim.Optimizer):
             "eps": eps,
         }
 
-        if lr < 0 or any(0 > beta or 1 < beta for beta in betas) or eps < 0:
+        if lr <= 0 or any(0 >= beta or 1 <= beta for beta in betas) or eps <= 0:
             raise ValueError("Invalid hyperparameters")
 
         super().__init__(params, defaults)
-        self.state = defaultdict()
         return
 
 
     def step(self) -> None:
 
-        for p in self.params:
-            if not self.state[p]:
-                self.state[p] = {
-                    "exp_avg": torch.zeros_like(p),
-                    "exp_avg_sq": torch.zeros_like(p),
-                    "step": 0
-                }
-            
-            grad = p.grad
+        for group in self.param_groups:
+            for p in group["params"]:
+                state = self.state[p]
+                if len(state) == 0:
+                    self.state[p] = {
+                        "exp_avg": torch.zeros_like(p),
+                        "exp_avg_sq": torch.zeros_like(p),
+                        "step": 0
+                    }
+
+                if p.grad is None:
+                    continue
+
+                self.state[p]["step"] += 1
+                
+                lr = group["lr"]
+                beta_1, beta_2 = group["betas"]
+                eps = group["eps"]
+                
+                grad = p.grad
+                m = self.state[p]["exp_avg"] * beta_1 + (1 - beta_1) * grad
+                v = self.state[p]["exp_avg_sq"] * beta_2 + (1 - beta_2) * grad ** 2
+
+                t = self.state[p]["step"]
+                a = lr * (math.sqrt(1 - beta_2 ** t)/(1 - beta_1 ** t))
+
+                with torch.no_grad():
+                    p -= a * m / (torch.sqrt(v) + eps)
+                    self.state[p]["exp_avg"] = m
+                    self.state[p]["exp_avg_sq"] = v
 
         return
