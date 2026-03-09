bf33711c5a9c4d3d1120ddf5966d6a84f0240e90
dkoe00 <darius.koenig@outlook.de>
2026-03-09 17:49:43 +0100

    :

---
+++ tests/test_state_dict.py
@@ -0,0 +1,27 @@
+import pytest
+import torch
+
+import miniadam
+
+
+test_params = [torch.randn(10,i) for i in range(5)]
+test_adam = miniadam(test_params)
+
+def simulate_gradients(params: torch.Tensor) -> None:
+    params.grad == torch.randn_like(params)
+    return
+
+
+def test_before_first_step():
+    assert len(test_adam.state) == 0
+
+
+def test_after_steps():
+    for group in test_adam.param_groups:
+        simulate_gradients(group)
+
+    test_adam.step()
+
+    state_dict = test_adam.state_dict()
+
+    #TODO @dkoe00: add actual tests
